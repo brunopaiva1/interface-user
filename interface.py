5fbae7eca85d5cc104cc217e1ee2a7e7f2b03843
brunopaiva1 <brunopaivamaster10@gmail.com>
2022-12-03 22:58:32 -0300

    second commit

--- interface.py
+++ interface.py
@@ -5,7 +5,8 @@ sg.theme('DarkPurple6') #tema
 
 layout = [
     [sg.Text('Usuário'), sg.Input(key='usuario', size=(20, 1))], #layout definido para o usuario
-    [sg.Text('Senha'), sg.Input(key='senha', password_char='*', size=(20,1))], #layout definido para senha
+    [sg.Text('Senha'), sg.Input(key='senha', password_char='*', size=(20,1))],
+    [sg.Text('Email'), sg.Input(key='email', size=(20, 1))], #layout definido para senha
     [sg.Checkbox('Deseja salvar as informações de login?')], #checkbox definido para salvar informações de login
     [sg.Button('Login')] #butão definido para login
 ]
@@ -19,5 +20,5 @@ while True:
     if eventos == sg.WINDOW_CLOSED: #fechando janela
         break
     if eventos == 'Login':
-        if valores['usuario'] == 'brunopaiva' and valores['senha'] == 'python3': #se o usuario for o solicitado ira imprimir o testo ta linha a seguir
+        if valores['usuario'] == 'brunopaiva' and valores['senha'] == 'python3' and valores['email'] == 'brunopaiva@python.com': #se o usuario for o solicitado ira imprimir o testo ta linha a seguir
             print('Seja Bem-Vindo!')
